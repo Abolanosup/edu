a0a038befc3f8de6fa049c0276e41589cf41c4f7
Cornelio022 <118117415+Cornelio022@users.noreply.github.com>
2022-11-14 10:42:26 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5325291385:AAHWftRu9ikAjwi7HBlXhW9VL2jk8vJcAYM')
+BOT_TOKEN =  os.environ.get('bot_token','5489718635:AAFJvtYCYb0YLgE_l_NafS1NGdRGVuoz8QE')
 API_ID =  os.environ.get('api_id','18641760')
 API_HASH = os.environ.get('api_hash','b7b026ce9d1d36400c02dc21d8df53a3')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','countryandlifee').split(';countryandlifee')
-#ACCES_USERS = ('tl_admin_user','countryandlifee')
-#ACCES_USERS = os.environ.get(countryandlifee)
+ACCES_USERS = os.environ.get('tl_admin_user','Elpatron220').split(';Elpatron220')
+#ACCES_USERS = ('tl_admin_user','Elpatron220')
+#ACCES_USERS = os.environ.get(Elpatron220)
 PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFGGJJYGJKLHFKYEDIGDYJJEKEFKREEGLFDKLH'))
 
 if PROXY:
